55fd76820117736d2d6823de792f63a1e076853f
Kanishka Narjary <kanishkan14@gmail.com>
2020-10-13 17:22:01 +0530

    push notification setup

--- hiddenleafmob/App.js
+++ hiddenleafmob/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import messaging from '@react-native-firebase/messaging';
 import MainNav from './navigation/mainnav';
 
 const App = () => {
+  useEffect(() => {
+    // Get the device token
+    messaging()
+      .getToken()
+      .then((token) => {
+        console.log('FCM token: ' + token);
+      });
+
+    // If using other push notification providers (ie Amazon SNS, etc)
+    // you may need to get the APNs token instead for iOS:
+    // if(Platform.OS == 'ios') { messaging().getAPNSToken().then(token => { return saveTokenToDatabase(token); }); }
+
+    // Listen to whether the token changes
+    return messaging().onTokenRefresh((token) => {
+      console.log('FCM token: ' + token);
+    });
+  }, []);
   return <MainNav />;
 };
 
--- hiddenleafmob/containers/Home/home.js
+++ hiddenleafmob/containers/Home/home.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, View, Button} from 'react-native';
 import {useDispatch, useSelector} from 'react-redux';
 import actions from '../../actionTypes';
+import messaging from '@react-native-firebase/messaging';
 
 const Home = ({navigation}) => {
   const dispatch = useDispatch();
   const orders = useSelector((state) => state.order);
+
+  useEffect(() => {
+    messaging().onMessage(async (remoteMessage) => {
+      console.log(remoteMessage);
+    });
+  }, []);
 
   const handlelogout = () => {
     dispatch({type: actions.SIGNOUT_USER});
